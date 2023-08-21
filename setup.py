76deb0320686eb2ca82d0d7adc4d4dc90db8121e
Andrew S. Rosen <asrosen93@gmail.com>
2023-08-21 07:58:32 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ plugins_list = ["hpc = covalent_hpc_plugin.hpc"]
 setup_info = {
     "name": "covalent-hpc-plugin",
     "packages": find_packages("."),
-    "version": "0.0.2",
+    "version": "0.0.3",
     "maintainer": "Andrew S. Rosen",
     "url": "https://github.com/arosen93/covalent-hpc-plugin",
     "license": "GNU Affero GPL v3.0",
